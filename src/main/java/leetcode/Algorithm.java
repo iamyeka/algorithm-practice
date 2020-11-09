a266472c1dee6b0b7820ff008b4272be9ecb1923
wurongjun@duiba.com.cn <775105396@qq.com>
2020-11-09 14:12:43 +0800

    最长公共子序列

--- src/main/java/leetcode/Algorithm.java
+++ src/main/java/leetcode/Algorithm.java
@@ -119,11 +119,11 @@ public class Algorithm {
 
     /**
      * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
-     *
+     * <p>
      * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
-     *
+     * <p>
      * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
-     *
+     * <p>
      * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
      * 输出：7 -> 0 -> 8
      * 原因：342 + 465 = 807
@@ -182,9 +182,9 @@ public class Algorithm {
 
     /**
      * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
-     *
+     * <p>
      * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
-     *
+     * <p>
      * 注意：给定 n 是一个正整数。
      *
      * @param n
@@ -205,6 +205,39 @@ public class Algorithm {
 
         return dp[n];
 
+    }
+
+    /**
+     * 最长公共子序列
+     * 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+     * <p>
+     * 输入：text1 = "abcde", text2 = "ace"
+     * 输出：3
+     * 解释：最长公共子序列是 "ace"，它的长度为 3。
+     *
+     * @param str1
+     * @param str2
+     * @return
+     */
+    public int maxCommonSubStr(String str1, String str2) {
+        int length1 = str1.length(), length2 = str2.length();
+
+        int[][] dp = new int[length1 + 1][length2 + 1];
+
+        for (int i = 1; i <= str1.length(); i++) {
+            for (int j = 1; j <= str2.length(); j++) {
+                char c1 = str1.charAt(i - 1);
+                char c2 = str2.charAt(j - 1);
+
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        return dp[length1][length2];
     }
 
     public static void main(String[] args) {
